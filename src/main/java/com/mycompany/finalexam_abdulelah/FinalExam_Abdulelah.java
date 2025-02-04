6d16a38d2635ce7474564b29fe1d88eeeddf31c9
Abdulelah <ameflix@hotmail.com>
2025-02-05 00:02:54 +0300

    Initial commit: Maven project for FinalExam_Abdulelah

---
+++ src/main/java/com/mycompany/finalexam_abdulelah/FinalExam_Abdulelah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalexam_abdulelah;
+
+/**
+ *
+ * @author Meflix
+ */
+public class FinalExam_Abdulelah {
+
+    public static void main(String[] args) {
+        System.out.println("Abdulelah Qadah answered this question!");
+    }
+}
