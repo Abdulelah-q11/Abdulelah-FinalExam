7afdc7ecead6871af63be5b6a7c877c7fa814d0f
Abdulelah-q11 <185695974+Abdulelah-q11@users.noreply.github.com>
2025-02-05 00:05:01 +0300

    Update FinalExam_Abdulelah.java

--- src/main/java/com/mycompany/finalexam_abdulelah/FinalExam_Abdulelah.java
+++ src/main/java/com/mycompany/finalexam_abdulelah/FinalExam_Abdulelah.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.finalexam_abdulelah;
 
-/**
- *
- * @author Meflix
- */
+
 public class FinalExam_Abdulelah {
 
     public static void main(String[] args) {
